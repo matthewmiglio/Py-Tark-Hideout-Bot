0f2f69228299a358a0a3579d2eccd63832364b88
Matt Main_PC <matmigg0804@gmail.com>
2023-05-22 23:49:02 -0400

    adding prints to states.py

--- hideoutbot/bot/states.py
+++ hideoutbot/bot/states.py
@@ -42,37 +42,60 @@ def state_tree(state, logger, jobs):  # -> check_fuel
     # water state -> scav case -> medstation -> lavatory -> bitcoin -> workbench
 
     elif state == "water":
+        print("Entered water state")
+
         if "water" in jobs:
             state = handle_water_collector(logger)
         else:
             state = "scav_case"
 
+        print(f"State after water is {state}")
+
     elif state == "scav_case":
+        print("Entered scav_case state")
+
         state = "medstation"
 
+        print(f"State after scav_case is {state}")
+
     elif state == "medstation":
+        print("Entered medstation state")
         if "medstation" in jobs:
             state = handle_medstation(logger)
         else:
             state = "lavatory"
 
+        print(f"State after medstation is {state}")
+
     elif state == "lavatory":
+        print("Entered lavatory state")
+
         if "Lavatory" in jobs:
             state = handle_lavatory(logger)
         else:
             state = "bitcoin"
 
+        print(f"State after lavatory is {state}")
+
     elif state == "bitcoin":
+        print("Entered bitcoin state")
+
         if "Bitcoin" in jobs:
             state = handle_bitcoin_miner(logger)
         else:
             state = "workbench"
 
+        print(f"State after bitcoin is {state}")
+
     elif state == "workbench":
+        print("Entered workbench state")
+
         if "Workbench" in jobs:
             state = handle_workbench(logger)
         else:
             state = "water"
+
+        print(f"State after workbench is {state}")
 
     return state
 
