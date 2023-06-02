2a98b93c1c36c6790a173d66006f5a5f8970f25d
Matthew Miglio <matmigg0804@gmail.com>
2023-06-02 15:45:03 -0400

    fixing main

--- hideoutbot/__main__.py
+++ hideoutbot/__main__.py
@@ -250,7 +250,7 @@ def dummy_main():
     # handle_workbench(lo1gger=Logger())
 
 
-dummy_main()
+# dummy_main()
 
-# if __name__ == "__main__":
-#     main()
+if __name__ == "__main__":
+    main()
